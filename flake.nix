b9664483c1049467857ac1f51f299c0f6e541e7e
Chaewoon Kang <kchawoon@naver.com>
2025-11-30 13:53:22 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+	description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+	};
+
+	outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      spaceship = nixpkgs.lib.nixosSystem {
+        modules = [
+				  ./configuration.nix
+
+					home-manager.nixosModules.home-manager {
+				    home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
+
+						home-manager.users.chaewoon = import ./home.nix;
+					}
+				];
+			};
+    };
+	};
+}
---
+++ home.nix
@@ -0,0 +1,144 @@
+{ config, pkgs, ... }:
+{
+	home.username = "chaewoon";
+	home.homeDirectory = "/home/chaewoon";
+
+	home.packages = with pkgs; [
+		# editor
+		neovim
+		
+		# fetches
+		fastfetch
+		nitch
+		pfetch
+		onefetch
+
+		# archives
+		nnn
+		zip
+		xz
+		unzip
+		p7zip
+
+		# utils
+		ripgrep
+		jq
+		yq-go
+		eza
+		fzf
+		zoxide
+
+		# networking
+		mtr
+		iperf3
+		dnsutils
+		ldns
+		aria2
+		socat
+		nmap
+		ipcalc
+
+		# misc
+		cowsay
+		file
+		which
+		tree
+		gnused
+		gnutar
+		gawk
+		zstd
+		gnupg
+
+		# nix related
+		nix-output-monitor
+
+		# productivity
+		hugo
+		glow
+
+		# monitoring
+		btop
+		iotop
+		iftop
+
+		# syscall monitoring
+		lsof
+		strace
+		ltrace
+
+		# system tools
+		sysstat
+		lm_sensors
+		ethtool
+		dnsutils
+		pciutils
+		usbutils
+
+		# guis
+		hyprland
+
+		# shell configurations
+		zsh-fast-syntax-highlighting
+    zsh-autosuggestions		
+	];
+
+	# git configuration
+	programs.git = {
+		enable = true;
+		userName = "Chaewoon Kang";
+		userEmail = "kchawoon@naver.com";
+	};
+
+	# starship prompt
+	programs.starship = {
+		enable = true;
+		settings = {
+			add_newline = true;
+			truncate_to_repo = false;
+			time.disabled = false;
+			time.format = "$time ($style) ";
+			cpp.disabled = false;
+		};
+	};
+
+	# zsh configuration
+	programs.zsh = {
+		enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+
+		history.size = 10000;
+		history.path = "$HOME/.zsh_history";
+    history.ignoreAllDups = true;
+
+		oh-my-zsh = {
+			enable = true;
+			plugins = [
+				"git"
+				"zoxide"
+			];
+		};
+
+    initContent = ''
+      source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
+    '';
+	};
+
+	# Ghostty
+	programs.ghostty = {
+    enable = true;
+		settings = {
+      font-size = 11;
+			font-thicken = "true";
+			font-thicken-strength = "10";
+			theme = "catppuccin-frappe";
+			cursor-style = "block";
+			background-opacity = "0.9";
+
+			window-padding-x = "2, 2";
+			window-padding-y = "2, 2";
+		};
+	};
+
+	home.stateVersion = "25.05";
+}
