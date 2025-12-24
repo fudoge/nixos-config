ae839f90b56c9102aa8688280912b464a06a35ee
Chaewoon Kang <kchawoon@naver.com>
2025-12-25 00:19:57 +0900

    Refactoring for multiple hosts...

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       spaceship = nixpkgs.lib.nixosSystem {
 
         modules = [
-				  ./configuration.nix
+				  ./hosts/thinkpad-e14-gen6/configuration.nix
 					home-manager.nixosModules.home-manager {
 				    home-manager.useGlobalPkgs = true;
 						home-manager.useUserPackages = true;
