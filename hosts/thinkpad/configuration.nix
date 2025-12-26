01c94d1991f1182a78babd7e37039761b83c47ff
Chaewoon Kang <kchawoon@naver.com>
2025-12-26 22:16:30 +0900

    Added keyboard fn keys

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -125,6 +125,7 @@
     libgtop
     libsoup_3
     gtksourceview3
+    spotify
   ];
 
   environment.variables.EDITOR = "vim";
--- modules/guis/hypr/hyprland/default.nix
+++ modules/guis/hypr/hyprland/default.nix
@@ -17,6 +17,22 @@
       bind = [
         "$mainMod, Return, exec, $terminal"
       ];
+      # Laptop multimedia keys and LCD brightness
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 est 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 est 5%-"
+      ];
+      # playerctl
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+      ];
     };
   };
 }
