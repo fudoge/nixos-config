1ef621a2269abf0ab104b35766650246901eecdb
Chaewoon Kang <kchawoon@naver.com>
2025-12-26 22:36:37 +0900

    Hyprland: Added workspace switch

--- modules/guis/hypr/hyprland/default.nix
+++ modules/guis/hypr/hyprland/default.nix
@@ -15,7 +15,42 @@
         "hyprpanel"
       ];
       bind = [
+        # basic binds..
         "$mainMod, Return, exec, $terminal"
+        "$mainMod, Q, killactive"
+        "$mainMod, M, exit"
+        "$mainMod, E, exec, $filemanager"
+        "$mainMod, V, togglefloating"
+        "$mainMod, L, exec, wlogout"
+        "$mainMod, F, fullscreen, 0"
+
+        # Switch workspace with mainMod + [0-9]
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
       ];
       # Laptop multimedia keys and LCD brightness
       bindel = [
@@ -32,6 +67,10 @@
         ", XF86AudioPause, exec, playerctl play-pause"
         ", XF86AudioPlay, exec, playerctl play-pause"
         ", XF86AudioPrev, exec, playerctl previous"
+      ];
+      windowrule = [
+        "suppressevent maximize, class:.*"
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
     };
   };
