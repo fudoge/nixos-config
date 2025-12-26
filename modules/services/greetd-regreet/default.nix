0071aad784777d0652ff438825ed543d8d6084f2
Chaewoon Kang <kchawoon@naver.com>
2025-12-26 17:43:49 +0900

    Adding ReGreet

--- modules/services/greetd-regreet/default.nix
+++ modules/services/greetd-regreet/default.nix
@@ -5,5 +5,30 @@
   ...
 }:
 {
-  programs.regreet.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.regreet}/bin/regreet";
+        user = "greeter";
+      };
+      initial_session = {
+        command = "Hyprland";
+        user = "chaewoon";
+      };
+    };
+  };
+
+  environment.etc."greetd/environments".text = ''
+    Hyprland
+  '';
+
+  environment.systemPackages = with pkgs; [
+    adw-gtk3
+    papirus-icon-theme
+  ];
+
+  environment.variables = {
+    GTK_THEME = "adw-gtk3-dark";
+  };
 }
