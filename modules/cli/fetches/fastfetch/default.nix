5e4af6064cce1e5dfa0c3c7c55408814fb487146
Chaewoon Kang <kchawoon@naver.com>
2025-12-26 22:56:37 +0900

    modifying fastfetch image

--- modules/cli/fetches/fastfetch/default.nix
+++ modules/cli/fetches/fastfetch/default.nix
@@ -5,6 +5,9 @@
   withGui ? false,
   ...
 }:
+let
+  logoImage = ../../../assets/logo.png;
+in
 {
   programs.fastfetch = {
     enable = true;
@@ -13,12 +16,37 @@
         if withGui then
           {
             type = "kitty";
-            source = "nixos";
+            source = "${logoImage}";
           }
         else
           {
             type = "small";
           };
+      modules = [
+        "title"
+        "seperator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "shell"
+        "display"
+        "wm"
+        "wm theme"
+        "font"
+        "cursor"
+        "terminal"
+        "terminal font"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "disk"
+        "local ip"
+        "battery"
+        "locale"
+      ];
     };
   };
 }
