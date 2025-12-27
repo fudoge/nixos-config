64b05a1159c4813c61da7756f83ff9aee422d1c9
Chaewoon Kang <kchawoon@naver.com>
2025-12-27 23:17:04 +0900

    Hyprpanel setup...

--- modules/guis/hypr/hyprpanel/default.nix
+++ modules/guis/hypr/hyprpanel/default.nix
@@ -7,6 +7,9 @@
 {
   programs.hyprpanel = {
     enable = true;
+    systemd.enable = true;
+    overwrite.enable = true;
+    theme = "catppuccin_frappe";
     settings = {
       theme.font = {
         name = "JetBrains Mono NF";
