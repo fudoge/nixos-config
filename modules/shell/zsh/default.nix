c40e624d800ba68e95df2f48858080320366b113
Chaewoon Kang <kchawoon@naver.com>
2026-01-01 16:22:57 +0900

    Switching to NixOS 25.11

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -33,7 +33,7 @@
     };
 
     initContent = ''
-      source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
+      source ${pkgs.fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
     '';
   };
 }
