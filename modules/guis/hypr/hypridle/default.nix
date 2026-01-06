af286d3ca1fbf2aef4eba3d5c10264a0e4438369
Chaewoon Kang <kchawoon@naver.com>
2026-01-07 01:51:37 +0900

    fixing hypridle

--- modules/guis/hypr/hypridle/default.nix
+++ modules/guis/hypr/hypridle/default.nix
@@ -9,34 +9,34 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        lock_cmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+        after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       };
 
       listener = [
         {
           timeout = 150;
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
+          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
+          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
         }
         {
           timeout = 150;
-          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-          on-resume = "brihgtnessctl -sd rgb:kbd_backlight";
+          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "${pkgs.brightnessctl}/bin/brihgtnessctl -sd rgb:kbd_backlight";
         }
         {
           timeout = 300;
-          on-timeout = "loginctl lock-session";
+          on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
         }
         {
           timeout = 330;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+          on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+          on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on && brightnessctl -r";
         }
         {
           timeout = 1800;
-          on-timeout = "systemctl suspend";
+          on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
         }
       ];
     };
--- modules/guis/hypr/hyprlock/default.nix
+++ modules/guis/hypr/hyprlock/default.nix
@@ -26,51 +26,6 @@
           vibrancy_drakness = 0.0;
         }
       ];
-
-      input-field = [
-        {
-          monitor = "";
-          size = "20%, 5%";
-          outline_thickness = 3;
-
-          inner_color = "rgba(0,0,0,0.0)";
-          dots_size = 0.33;
-          dots_spacing = 0.15;
-          dots_center = true;
-          dots_rounding = -1;
-          hide_input = false;
-
-          fade_on_empty = false;
-          rounding = 15;
-
-          position = "0, -60";
-          halign = "center";
-          valign = "center";
-
-          placeholder_text = "<i>Input Password to Unlock...</i>";
-        }
-      ];
-
-      labels = [
-        {
-          monitor = "";
-          text = "$TIME";
-          font_size = 80;
-          font_family = "JetBrainoMono Nerd Font";
-          position = "0, 160";
-          halign = "center";
-          valign = "center";
-        }
-        {
-          monitor = "";
-          text = "cmd[update:60000] date '+%A, %B %d'";
-          font_size = 18;
-          font_family = "JetBrainoMono Nerd Font";
-          position = "0, 40";
-          halign = "center";
-          valign = "center";
-        }
-      ];
     };
   };
 }
