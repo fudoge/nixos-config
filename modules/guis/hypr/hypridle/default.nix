fe7e2f4d0541218609aa57b23885504327425af6
Chaewoon Kang <kchawoon@naver.com>
2026-01-07 01:10:24 +0900

    fixing hypridle

--- modules/guis/hypr/hypridle/default.nix
+++ modules/guis/hypr/hypridle/default.nix
@@ -9,21 +9,21 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        lock_cmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+        after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       };
 
       listener = [
         {
           timeout = 150;
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
+          on-timeout = "${pkgs.brihgtnessctl}/bin/brightnessctl -s set 10";
+          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
         }
         {
           timeout = 150;
-          on-timeout = "brihtnessctl -sd rgb:kbd_backlight set 0";
-          on-resume = "brihtnessctl -sd rgb:kbd_backlight";
+          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "${pkgs.brightnessctl}/bin/brihgtnessctl -sd rgb:kbd_backlight";
         }
         {
           timeout = 300;
