8922679923d64f1a23d6225b6e53d256edac914b
Chaewoon Kang <kchawoon@naver.com>
2026-01-10 01:49:22 +0900

    Added icon

--- nvf-module.nix
+++ nvf-module.nix
@@ -508,10 +508,10 @@
           },
           signs = {
             text = {
-              [vim.diagnostic.severity.ERROR] = "",
-              [vim.diagnostic.severity.WARN]  = "",
-              [vim.diagnostic.severity.INFO]  = "",
-              [vim.diagnostic.severity.HINT]  = "",
+              [vim.diagnostic.severity.ERROR] = "",
+              [vim.diagnostic.severity.WARN]  = "",
+              [vim.diagnostic.severity.INFO]  = "",
+              [vim.diagnostic.severity.HINT]  = "󰋼",
             },
             numhl = {
               [vim.diagnostic.severity.ERROR] = "ErrorMsg",
