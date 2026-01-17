e8de3ee76b3290f46ab48ebd8e32d435891bbd53
Chaewoon Kang <kchawoon@naver.com>
2026-01-17 12:18:21 +0900

    nvf: nvim-web-devicons

--- nvf-module.nix
+++ nvf-module.nix
@@ -55,50 +55,53 @@
         style = "frappe";
       };
 
-      visuals.indent-blankline = {
-        enable = true;
+      visuals = {
+        nvim-web-devicons.enable = true;
+        indent-blankline = {
+          enable = true;
 
-        setupOpts = {
-          debounce = 100;
+          setupOpts = {
+            debounce = 100;
 
-          indent = {
-            char = "│";
-            tab_char = "▏";
-            smart_indent_cap = true;
-            repeat_linebreak = true;
-            priority = 1;
-            highlight = [
-              "IblIndent"
-            ];
-          };
+            indent = {
+              char = "│";
+              tab_char = "▏";
+              smart_indent_cap = true;
+              repeat_linebreak = true;
+              priority = 1;
+              highlight = [
+                "IblIndent"
+              ];
+            };
 
-          scope = {
-            enabled = true;
-            char = "┃";
-            show_start = false;
-            show_end = false;
-            highlight = [
-              "IblScope"
-            ];
-            priority = 1024;
+            scope = {
+              enabled = true;
+              char = "┃";
+              show_start = false;
+              show_end = false;
+              highlight = [
+                "IblScope"
+              ];
+              priority = 1024;
 
-            exclude = {
-              language = ["help" "dashboard" "alpha"];
-              node_type = {
-                "*" = ["source_file" "program"];
-                lua = ["chunk"];
-                python = ["module"];
+              exclude = {
+                language = ["help" "dashboard" "alpha"];
+                node_type = {
+                  "*" = ["source_file" "program"];
+                  lua = ["chunk"];
+                  python = ["module"];
+                };
               };
             };
-          };
 
-          whitespace = {
-            remove_blankline_trail = true;
-          };
+            whitespace = {
+              remove_blankline_trail = true;
+            };
 
-          viewport_buffer = {
-            min = 30;
-            max = 300;
+            viewport_buffer = {
+              min = 30;
+              max = 300;
+            };
           };
         };
       };
