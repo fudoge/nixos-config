87f8fa394ecd303a4f69063dd095e665be5c1848
Chaewoon Kang <kchawoon@naver.com>
2026-04-30 23:42:11 +0900

    audio utilities

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -102,11 +102,17 @@
   # OR
   services.pipewire = {
     enable = true;
+    audio.enable = true;
+    pulse.enable = true;
     wireplumber.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    jack.enable = true;
   };
   security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
 
   # bluetooth
   services.blueman.enable = true;
@@ -201,8 +207,13 @@
     # hypr
     hyprpolkitagent
 
-    # Spatial audio
+    # audio
+    pauvcontrol
+    helvum
+    qpwgraph
     easyeffects
+    plmixer
+    playerctl
 
     # gui apps
     spotify
