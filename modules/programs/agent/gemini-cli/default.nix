5294c8b89db45a01ff6749a211c13cddc13670d8
Chaewoon Kang <kchawoon@naver.com>
2026-05-01 15:51:08 +0900

    fix: gemini cli auth

--- modules/programs/agent/gemini-cli/default.nix
+++ modules/programs/agent/gemini-cli/default.nix
@@ -8,5 +8,23 @@
   programs.gemini-cli = {
     enable = true;
     package = unstable.gemini-cli;
+    settings = {
+      general = {
+        preferredEditor = "nvim";
+        previewFeatures = true;
+        vimMode = true;
+      };
+      ide = {
+        enabled = true;
+      };
+      privacy = {
+        usageStatisticsEnabled = false;
+      };
+      security = {
+        auth = {
+          selectedType = "oauth-personal";
+        };
+      };
+    };
   };
 }
