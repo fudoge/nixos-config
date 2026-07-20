79c5a19747ebfb7d097db44e030ec4063f84a031
Chaewoon Kang <kchawoon@naver.com>
2026-07-20 23:05:22 +0900

    add steam

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -191,6 +191,13 @@
 
   programs.kdeconnect.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   environment.variables = {
     EDITOR = "vim";
     QT_ICON_THEME = "Papirus-Dark";
