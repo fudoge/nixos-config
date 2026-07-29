ecadbc6850dcc4a4cd0bf1b243b6e998e279a06f
Chaewoon Kang <kchawoon@naver.com>
2026-07-29 23:48:49 +0900

    nvf: add notify-nvim bg color

--- modules/programs/editors/nvf/default.nix
+++ modules/programs/editors/nvf/default.nix
@@ -599,7 +599,9 @@ in {
           package = pkgs.vimPlugins.nvim-notify;
           setup = ''
             local notify = require("notify")
-            notify.setup({})
+            notify.setup({
+              background_colour = "#000000",
+            })
             vim.notify = notify
           '';
         };
