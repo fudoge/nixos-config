55cdf0cb0b591acd5b294c5be1f198c2789fe1b2
Chaewoon Kang <kchawoon@naver.com>
2026-08-28 00:38:24 +0900

    clawd-on-desk: update to 0.16.0

--- modules/programs/clawd-on-desk/default.nix
+++ modules/programs/clawd-on-desk/default.nix
@@ -5,11 +5,11 @@
 }: let
   clawd-on-desk = pkgs.appimageTools.wrapType2 rec {
     pname = "clawd-on-desk";
-    version = "0.15.0";
+    version = "0.16.0";
 
     src = pkgs.fetchurl {
       url = "https://github.com/rullerzhou-afk/clawd-on-desk/releases/download/v${version}/Clawd-on-Desk-${version}-x86_64.AppImage";
-      hash = "sha256-uZMrLnaKRaBniveEo4SrCCVx2x3D5F2NdN2JKBv1s1E=";
+      hash = "sha256-QUqEYW691fY5VxKpfp1RbhimAd35Y6nSLeF59WnIBgw=";
     };
   };
 in {
